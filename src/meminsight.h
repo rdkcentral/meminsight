a7b0fb392c9caaa085c0f886abfa2e760c1f9258
Saranya <saranya_elango@comcast.com>
2026-08-10 09:41:56 +0000

    Fix for Build errors

--- src/meminsight.h
+++ src/meminsight.h
@@ -264,7 +264,7 @@ int isPID(const char *str);
 int getPIDByProcessName(const char *procName, unsigned int *pidOut);
 int parseConfig(const char *configFile, Config_Data *config);
 int collectSystemMemoryStats(bool enableKThreads, const char *outDir, int iterations, int interval, bool long_run, bool upload_enabled, int upload_interval, const char *upload_url);
-int handleConfigMode(const char *confFile, const char *cli_out_dir, bool cli_output_set, int cli_iterations, int cli_interval, bool enableKThreads, bool long_run, bool upload_enabled, int upload_interval, const char *upload_url)
+int handleConfigMode(const char *confFile, const char *cli_out_dir, bool cli_output_set, int cli_iterations, int cli_interval, bool enableKThreads, bool long_run, bool upload_enabled, int upload_interval, const char *upload_url);
 int fillProcessStatFields(unsigned pid, Process_Info *info, unsigned *flagsOut);
 
 #ifdef ENABLE_CJSON
